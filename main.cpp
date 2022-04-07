fe626fde59cd675b19ddf18b6523cdcc7e31e261
Garr777 <redka17@mail.ru>
2022-04-08 00:28:16 +0300

    HW by Sabirov Rishat

--- main.cpp
+++ main.cpp
@@ -53,18 +53,185 @@ char* resize(char* str, unsigned size, unsigned new_size)
     delete[] str;
 	return newStr;
 }
-/*
-class Rational 
+
+
+class Rational
 {
-...
-private:                        в процессе разработки
-	int numerator_;
-	int denominator_;
-}
-*/
+	private:
+		int numerator_;
+		int denominator_;
+	public:
+		Rational()
+		{}
+
+		Rational(int numerator, int denominator) : numerator_(numerator), denominator_(denominator)
+		{
+			if(numerator_ > denominator_) // Используем Алгоритм Евклида для нахождения НОД
+			{
+				int a = numerator_;
+				int b = denominator_;
+				while(a != b && a != 0)
+				{
+					if(a > b)
+					{
+						a -= b;
+					}
+					else 
+					{
+						b -= a;
+					}
+				}
+				if(a == 0)
+				{
+					numerator_ /= denominator_;
+					denominator_ = 1;
+				}
+				else
+				{
+					numerator_ /= a;
+					denominator_ /= a;
+				}
+			}
+		}
+		//////////////////Арифметические операторы операторы с double 
+		Rational operator * (double num)
+		{
+			Rational result(numerator_ * num, denominator_);
+			return result;
+		}
+		Rational operator / (double num)
+		{
+			Rational result(numerator_, denominator_ * num);
+			return result;
+		}
+
+		Rational operator + (double num)
+		{
+			Rational result(numerator_ + num * denominator_ , denominator_ );
+			return result;
+		}
+
+		Rational operator - (double num)
+		{
+			Rational result(numerator_ + num * denominator_ , denominator_ );
+			return result;
+		}
+		//////////////////Арифметические операторы операторы с Rational
+		Rational operator * (Rational r)
+		{
+			Rational result(numerator_ * r.numerator_, denominator_ * r.denominator_);
+			return result;
+		}
+		Rational operator / (Rational r)
+		{
+			Rational result(numerator_ * r.denominator_ , denominator_ * r.numerator_);
+			return result;
+		}
+
+		Rational operator + (Rational r)
+		{
+			Rational result(numerator_  * r.denominator_  + r.numerator_ * denominator_, denominator_ * r.denominator_);
+			return result;
+		}
+
+		Rational operator - (Rational r)
+		{
+			Rational result(numerator_  * r.denominator_  - r.numerator_ * denominator_, denominator_ * r.denominator_);
+			return result;
+		}
+		//////////////////Инкременты и декременты
+		Rational operator ++ () //Префиксный инкремент
+		{
+			Rational result(numerator_ +  denominator_ , denominator_ );
+			return result;
+		}
+
+		Rational operator++ (int)//Постфиксный инкремент
+		{
+			Rational prev = *this;
+			++*this;
+			return prev;
+		}
+
+		Rational Rational::operator -- ()//Префиксный декремент
+		{
+			Rational result(numerator_ -  denominator_ , denominator_ );
+			return result;
+		}
+
+		Rational operator-- (int)//Постфиксный декремент
+		{
+			Rational prev = *this;
+			--*this;
+			return prev;
+		}
+		//////////////////Операторы сравнения с Rational
+		bool operator == (Rational c2)
+		{
+			return numerator_ * c2.denominator_ == denominator_ * c2.numerator_;
+		}
+
+		bool operator != (Rational c2)
+		{
+			return numerator_ * c2.denominator_ != denominator_ * c2.numerator_;
+		}
+
+		bool operator > (Rational c2)
+		{
+			return numerator_ * c2.denominator_ > denominator_ * c2.numerator_;
+		}
+
+		bool operator >= (Rational c2)
+		{
+			return numerator_ * c2.denominator_ >= denominator_ * c2.numerator_;
+		}
+
+		bool operator < (Rational c2)
+		{
+			return numerator_ * c2.denominator_ < denominator_ * c2.numerator_;
+		}
+
+		bool operator <= (Rational c2)
+		{
+			return numerator_ * c2.denominator_ <= denominator_ * c2.numerator_;
+		}
+
+		//////////////////Операторы сравнения с целочисленными значениями
+		bool operator == (int n)
+		{
+			return numerator_  == denominator_ * n;
+		}
+
+		bool operator != (int n)
+		{
+			return numerator_  != denominator_ * n;
+		}
+
+		bool operator > (int n)
+		{
+			return numerator_  > denominator_ * n;
+		}
+
+		bool operator >= (int n)
+		{
+			return numerator_  >= denominator_ * n;
+		}
+
+		bool operator < (int n)
+		{
+			return numerator_  < denominator_ * n;
+		}
+
+		bool operator <= (int n)
+		{
+			return numerator_  <= denominator_ * n;
+		}
+};
+
+
 int main()
 {
-    int length = 5;
+	int length = 5;
     int width = 5;
     int** array = new int*[length]; 
     for (int i = 0; i < length; i++)
